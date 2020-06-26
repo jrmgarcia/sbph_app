1f353d061bbbc08b5df53a9e5c59e46ba8cba80c
John Russel Garcia <jmgarcia9@up.edu.ph>
2020-06-26 13:17:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sbph_app
+ Simple login, signup, and home page
---
+++ android/app/src/main/kotlin/com/example/sbph_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sbph_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
